bd78d890a2ac0b4f73680653096f350945cef582
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-10-28 11:29:08 +0200

    params as dataclass

--- EspressoMD/espresso_md.py
+++ EspressoMD/espresso_md.py
@@ -3,35 +3,44 @@ import numpy as np
 import h5py
 import os
 import logging
+import dataclasses
+import pint
+
+
+@dataclasses.dataclass()
+class MDParams:
+    n_colloids: int
+    ureg: pint.UnitRegistry
+    box_length: pint.Quantity
+    colloid_radius: pint.Quantity
+    colloid_density: pint.Quantity
+    fluid_dyn_viscosity: pint.Quantity
+    WCA_epsilon: pint.Quantity
+    temperature: pint.Quantity
+    time_step: pint.Quantity
+    time_slice: pint.Quantity
+    write_interval: pint.Quantity
 
 
 class EspressoMD:
-    def __init__(self, md_params, seed=42, out_folder='.', loglevel=logging.DEBUG):
+    def __init__(self, md_params, seed=42, out_folder='.', loglevel=logging.DEBUG, write_chunk_size=1):
         self.params = md_params
         self.out_folder = out_folder
         self.seed = seed
 
-        self._init_h5_output()
+        self._init_unit_system()
+        self._init_h5_output(write_chunk_size)
         self._init_logger(loglevel)
 
         self.system = System(box_l=3 * [1])
         self.colloids = list()
 
     def setup_simulation(self):
-        ureg = self.params['ureg']
-        # define simulation units
-        ureg.define('sim_length = 1e-6 meter')
-        ureg.define('sim_time = 1 second')
-        ureg.define(f"sim_energy = 293 kelvin * boltzmann_constant")
-
-        ureg.define('sim_mass = sim_energy * sim_time**2 / sim_length**2')
-        ureg.define('sim_dyn_viscosity = sim_mass / (sim_length * sim_time)')
-
         # parameter unit conversion
-        time_step = self.params['time_step'].m_as('sim_time')
+        time_step = self.params.time_step.m_as('sim_time')
         # time slice: the amount of time the integrator runs before we look at the configuration and change forces
-        time_slice = self.params['time_slice'].m_as('sim_time')
-        box_l = np.array(3 * [self.params['box_length'].m_as('sim_length')])
+        time_slice = self.params.time_slice.m_as('sim_time')
+        box_l = np.array(3 * [self.params.box_length.m_as('sim_length')])
 
         # system setup. Skin is a verlet list parameter that has to be set, but only affects performance
         self.system.box_l = box_l
@@ -39,18 +48,18 @@ class EspressoMD:
         self.system.cell_system.skin = 0.4
         particle_type = 0
 
-        colloid_radius = self.params['colloid_radius'].m_as('sim_length')
+        colloid_radius = self.params.colloid_radius.m_as('sim_length')
         self.system.non_bonded_inter[particle_type, particle_type].wca.set_params(
             sigma=(2 * colloid_radius) ** (-1 / 6),
-            epsilon=self.params['WCA_epsilon'].m_as(
+            epsilon=self.params.WCA_epsilon.m_as(
                 'sim_energy'))
 
         # set up the particles. The handles will later be used to calculate/set forces
         rng = np.random.default_rng(self.seed)
-        colloid_mass = (4. / 3. * np.pi * self.params['colloid_radius'] ** 3 * self.params['colloid_density']).m_as(
+        colloid_mass = (4. / 3. * np.pi * self.params.colloid_radius ** 3 * self.params.colloid_density).m_as(
             'sim_mass')
         colloid_rinertia = 2. / 5. * colloid_mass * colloid_radius ** 2
-        for _ in range(self.params['n_colloids']):
+        for _ in range(self.params.n_colloids):
             start_pos = box_l * rng.random((3,))
             # http://mathworld.wolfram.com/SpherePointPicking.html
             theta, phi = [np.arccos(2. * rng.random() - 1), 2. * np.pi * rng.random()]
@@ -64,9 +73,9 @@ class EspressoMD:
                                            rotation=3 * [True])
             self.colloids.append(colloid)
 
-        colloid_friction_translation = 6 * np.pi * self.params['fluid_dyn_viscosity'].m_as(
+        colloid_friction_translation = 6 * np.pi * self.params.fluid_dyn_viscosity.m_as(
             'sim_dyn_viscosity') * colloid_radius
-        colloid_friction_rotation = 8 * np.pi * self.params['fluid_dyn_viscosity'].m_as(
+        colloid_friction_rotation = 8 * np.pi * self.params.fluid_dyn_viscosity.m_as(
             'sim_dyn_viscosity') * colloid_radius ** 3
 
         # remove overlap
@@ -75,7 +84,7 @@ class EspressoMD:
         self.system.integrator.run(1000)
 
         # set the brownian thermostat
-        kT = (self.params['temperature'] * ureg.boltzmann_constant).m_as('sim_energy')
+        kT = (self.params.temperature * self.ureg.boltzmann_constant).m_as('sim_energy')
         self.system.thermostat.set_brownian(kT=kT,
                                             gamma=colloid_friction_translation,
                                             gamma_rotation=colloid_friction_rotation,
@@ -84,11 +93,22 @@ class EspressoMD:
 
         # set integrator params
         steps_per_slice = int(round(time_slice / time_step))
-        self.params.update({'steps_per_slice': steps_per_slice})
+        self.params.steps_per_slice = steps_per_slice
         if abs(steps_per_slice - time_slice / time_step) > 1e-10:
             raise ValueError('inconsistent parameters: time_slice must be integer multiple of time_step')
 
-    def _init_h5_output(self):
+    def _init_unit_system(self):
+        self.ureg = self.params.ureg
+
+        self.ureg.define('sim_length = 1e-6 meter')
+        self.ureg.define('sim_time = 1 second')
+        self.ureg.define(f"sim_energy = 293 kelvin * boltzmann_constant")
+
+        self.ureg.define('sim_mass = sim_energy * sim_time**2 / sim_length**2')
+        self.ureg.define('sim_dyn_viscosity = sim_mass / (sim_length * sim_time)')
+
+    def _init_h5_output(self, write_chunk_size):
+        self.write_chunk_size = write_chunk_size
         self.h5_filename = self.out_folder + '/trajectory.hdf5'
         os.makedirs(self.out_folder, exist_ok=True)
         self.traj_holder = {'Times': list(),
@@ -106,15 +126,15 @@ class EspressoMD:
                                        dtype=float,
                                        **dataset_kwargs)
             part_group.require_dataset('Unwrapped_Positions',
-                                       shape=(self.params['n_colloids'], traj_len, 3),
+                                       shape=(self.params.n_colloids, traj_len, 3),
                                        dtype=float,
                                        **dataset_kwargs)
             part_group.require_dataset('Velocities',
-                                       shape=(self.params['n_colloids'], traj_len, 3),
+                                       shape=(self.params.n_colloids, traj_len, 3),
                                        dtype=float,
                                        **dataset_kwargs)
             part_group.require_dataset('Directors',
-                                       shape=(self.params['n_colloids'], traj_len, 3),
+                                       shape=(self.params.n_colloids, traj_len, 3),
                                        dtype=float,
                                        **dataset_kwargs)
         self.write_idx = 0.
@@ -155,19 +175,19 @@ class EspressoMD:
 
     def integrate_system(self, n_slices, force_rule):
         for _ in range(n_slices):
-            if self.system.time >= self.params['write_interval'].m_as('sim_time') * self.write_idx:
+            if self.system.time >= self.params.write_interval.m_as('sim_time') * self.write_idx:
                 self.traj_holder['Times'].append(self.system.time)
                 self.traj_holder['Unwrapped_Positions'].append(np.stack([c.pos for c in self.colloids]))
                 self.traj_holder['Velocities'].append(np.stack([c.v for c in self.colloids]))
                 self.traj_holder['Directors'].append(np.stack([c.director for c in self.colloids]))
 
-                if len(self.traj_holder['Times']) >= self.params['write_chunk_size']:
+                if len(self.traj_holder['Times']) >= self.write_chunk_size:
                     self._write_chunk_to_file()
                     for val in self.traj_holder.values():
                         val.clear()
                     self.write_idx += 1
 
-            self.system.integrator.run(self.params['steps_per_slice'])
+            self.system.integrator.run(self.params.steps_per_slice)
             for coll in self.colloids:
                 force_on_colloid = force_rule.calc_force(coll, [c for c in self.colloids if c is not coll])
                 coll.ext_force = force_on_colloid
@@ -176,3 +196,6 @@ class EspressoMD:
         return {'Unwrapped_Positions': np.stack([c.pos for c in self.colloids]),
                 'Velocities': np.stack([c.v for c in self.colloids]),
                 'Directors': np.stack([c.director for c in self.colloids])}
+
+    def get_unit_system(self):
+        return self.ureg
--- sample_simulation.py
+++ sample_simulation.py
@@ -60,28 +60,24 @@ class ToCenterMass(MLModel):
 com_force_rule = ToCenterMass(1000, only_in_front=True)
 
 ureg = pint.UnitRegistry()
-
-params = {'n_colloids': 10,
-          'colloid_radius': ureg.Quantity(1, 'micrometer'),
-          'fluid_dyn_viscosity': ureg.Quantity(8.9, 'pascal * second'),
-          'WCA_epsilon': ureg.Quantity(1e-20, 'joule'),
-          'colloid_density': ureg.Quantity(2.65, 'gram / centimeter**3'),
-          'temperature': ureg.Quantity(300, 'kelvin'),
-          'box_length': ureg.Quantity(100, 'micrometer'),
-          'time_step': ureg.Quantity(0.05, 'second'),
-          'time_slice': ureg.Quantity(0.1, 'second'),
-          'write_interval': ureg.Quantity(0.5, 'second'),
-          'write_chunk_size': 1000,
-          'ureg': ureg
-          }
+params = espresso_md.MDParams(n_colloids=10,
+                              ureg=ureg,
+                              colloid_radius=ureg.Quantity(1, 'micrometer'),
+                              fluid_dyn_viscosity=ureg.Quantity(8.9, 'pascal * second'),
+                              WCA_epsilon=ureg.Quantity(1e-20, 'joule'),
+                              colloid_density=ureg.Quantity(2.65, 'gram / centimeter**3'),
+                              temperature=ureg.Quantity(300, 'kelvin'),
+                              box_length=ureg.Quantity(100, 'micrometer'),
+                              time_step=ureg.Quantity(0.05, 'second'),
+                              time_slice=ureg.Quantity(0.1, 'second'),
+                              write_interval=ureg.Quantity(0.5, 'second'))
 
 output_folder = './test_sim/'
 
-system_runner = espresso_md.EspressoMD(params, seed = 42, out_folder=output_folder)
+system_runner = espresso_md.EspressoMD(params, seed=42, out_folder=output_folder, write_chunk_size=1000)
 system_runner.setup_simulation()
 
 for i in range(10):
     system_runner.integrate_system(500, com_force_rule)
     data_for_ML_trainer = system_runner.get_particle_data()
     com_force_rule.train(data_for_ML_trainer)
-
