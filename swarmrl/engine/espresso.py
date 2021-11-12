actually fix the 2d rotation

swarmrl/engine/espresso.py
@@ -270,10 +270,9 @@ class EspressoMD(Engine):
                         rotation_angle = np.arccos(np.dot(new_direction, old_direction))
                         if rotation_angle > 1e-6:
                             rotation_axis = np.cross(old_direction, new_direction)
+                            rotation_axis /= np.linalg.norm(rotation_axis)
                             # only values of [0,0,1], [0,0,-1] can come out here, plusminus numerical errors
-                            rotation_axis[0] = 0
-                            rotation_axis[1] = 0
-                            rotation_axis[2] = round(rotation_axis[2])
+                            rotation_axis = [0, 0, round(rotation_axis[2])]
                             coll.rotate(axis=rotation_axis, angle=rotation_angle)
 
             self.system.integrator.run(self.params.steps_per_slice)

swarmrl/models/bechinger_models.py
@@ -96,7 +96,10 @@ class Baeuerle2020(InteractionModel):
 
     def calc_new_direction(self, colloid, other_colloids) -> np.ndarray:
         force = self._calc_force(colloid, other_colloids)
-        return force/np.linalg.norm(force)
+        if np.all(force == 0.):
+            return None
+        else:
+            return force/np.linalg.norm(force)
 
     def calc_torque(self, colloid, other_colloids) -> np.ndarray:
         return np.zeros((3,))