d39366a97fd8e4611a3f1cf9063a0a6e3ececc34
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2022-02-15 13:29:00 +0100

    clean up real_experiment

--- swarmrl/engine/real_experiment.py
+++ swarmrl/engine/real_experiment.py
@@ -1,8 +1,8 @@
 """
 Parent class for the engine.
 """
-from socket import socket
 import swarmrl.models.interaction_model
+import swarmrl.engine.engine
 import numpy as np
 import dataclasses
 import struct
@@ -15,6 +15,13 @@ class Colloid:
     id: int
 
 
+class ConnectionClosedError(Exception):
+    """
+    Exception to capture when Matlab closes the connection
+    """
+    pass
+
+
 experiment_actions = {
     "do_nothing": 1,
     "rotate_clockwise": 4,
@@ -23,44 +30,45 @@ experiment_actions = {
 }
 
 
-def _handle_one_step(
-    state: np.array,
-    force_model: swarmrl.models.interaction_model.InteractionModel,
-) -> np.array:
-    n_colloids = len(state)
-
-
-
-
 def vector_from_angle(angle):
     return np.array([np.cos(angle), np.sin(angle), 0])
 
 
-class RealExperiment:
+class RealExperiment(swarmrl.engine.engine.Engine):
     def __init__(self, connection):
         self.connection = connection
 
     def setup_simulation(self) -> None:
         pass
 
-    def receive_colloids(self, data_size):
+    def receive_colloids(self):
+        print("Waiting for receiving data_size")
+        data_size = self.connection.recv(8)
+        # break if connection closed
+        if not data_size:
+            print("Received connection closed signal")
+            raise ConnectionClosedError
+
         data_size_int = struct.unpack('I', data_size)[0]
+        print(f"Received data_size = {data_size_int}")
         print("Waiting for receiving actual data")
         data = self.connection.recv(8 * data_size_int)
         while data and len(data) < 8 * data_size_int:
             data.extend(self.connection.recv(8 * data_size_int))
 
         # cast bytestream to double array and reshape to [x y theta id]
-        data = np.array(struct.unpack(str(len(data)//8)+"d", data)).reshape((-1, 4))
+        data = np.array(struct.unpack(str(len(data) // 8) + "d", data)).reshape((-1, 4))
         print(f"Received data with shape {np.shape(data)} \n")
         colloids = []
         for row in data:
-            coll = Colloid(pos=[row[0],row[1],0], director=vector_from_angle(row[2]), id=row[3])
+            coll = Colloid(pos=np.array([row[0], row[1], 0]),
+                           director=vector_from_angle(row[2]),
+                           id=row[3])
             colloids.append(coll)
 
         return colloids
 
-    def get_actions(self, colloids, force_model):
+    def get_actions(self, colloids, force_model) -> np.array:
         n_colloids = len(colloids)
         ret = np.zeros((n_colloids, 2))
         for idx, coll in enumerate(colloids):
@@ -70,62 +78,40 @@ class RealExperiment:
             action = force_model.calc_action(coll, other_colloids)
 
             if not action.force == 0.0:
-                action_id=experiment_actions["be_active"]
+                action_id = experiment_actions["be_active"]
             else:
-                action_id=experiment_actions["do_nothing"]
+                action_id = experiment_actions["do_nothing"]
 
             if not np.all(action.torque == 0):
                 if action.torque[2] > 0:
-                    action_id=experiment_actions["rotate_anticlockwise"]
+                    action_id = experiment_actions["rotate_anticlockwise"]
                 else:
-                    action_id=experiment_actions["rotate_clockwise"]
+                    action_id = experiment_actions["rotate_clockwise"]
 
-            ret[idx,0] = coll.id
-            ret[idx,1] = action_id
+            ret[idx, 0] = coll.id
+            ret[idx, 1] = action_id
         return ret
 
-    def send_sctions(self, actions):
+    def send_actions(self, actions):
         # Flatten data in 'Fortran' style
         data = actions.flatten('F')
         print(f"Sending data with shape {np.shape(data)} \n")
-        # transform to bytes
-        data_bytes = struct.pack(str(len(data))+"d", *data)
 
-        # and send them (as bytestream)
+        data_bytes = struct.pack(str(len(data)) + "d", *data)
         self.connection.sendall(data_bytes)
 
     def integrate(
-        self,
-        n_slices: int,
-        force_model: swarmrl.models.interaction_model.InteractionModel,
+            self,
+            n_slices: int,
+            force_model: swarmrl.models.interaction_model.InteractionModel,
     ) -> None:
         for _ in range(n_slices):
-            print("Waiting for receiving data_size")
-            data_size = self.connection.recv(8)
-            if not data_size == b'': #received number of particles
-                print(f"Received data_size = {struct.unpack('I', data_size)[0]}")
-                colloids = self.receive_colloids(data_size)
-                actions = self.get_actions(colloids, force_model)
-                self.send_sctions(actions)
-            else:
-                #force_model.finalize()
-                print("Empty data, closing connection")
+            try:
+                colloids = self.receive_colloids()
+            except ConnectionClosedError:
+                # force_model.finalize()
                 self.connection.close()
                 break
 
-
-    def get_particle_data(self) -> dict:
-        """
-        Get position, velocity and director of the particles as a dict of np.array
-        """
-        state = self.communicator.get_particle_state()
-        n_colloids = len(state)
-        poss = np.column_stack(state[:, 0:2], np.zeros((n_colloids,)))
-        directors = np.stack([vector_from_angle(angle) for angle in state[:, 2]])
-        return {"Unwrapped_Positions": poss, "Directors": directors}
-
-    def finalize(self):
-        """
-        Optional: to clean up after finishing the simulation (e.g. writing the last chunks of trajectory)
-        """
-        pass
+            actions = self.get_actions(colloids, force_model)
+            self.send_actions(actions)
