5e7659e75909e935182845fe82c245a151fc7635
SamTov <tovey.samuel@gmail.com>
2022-02-21 15:15:24 +0100

    fix requirements.txt pip call.

--- CI/unit_tests/gyms/test_mlp_rl.py
+++ CI/unit_tests/gyms/test_mlp_rl.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from swarmrl.gyms.mlp_rl import MLPRL
 
+import torch
+
 
 class TestMLPRL:
     """
@@ -29,26 +31,36 @@ class TestMLPRL:
         """
         Test that the format data method is working correctly.
         """
+        a = torch.tensor(1., requires_grad=True)
+        b = torch.tensor(2., requires_grad=True)
+        c = torch.tensor(3., requires_grad=True)
+        d = torch.tensor(5., requires_grad=True)
+        e = torch.tensor(6., requires_grad=True)
+        f = torch.tensor(7., requires_grad=True)
+
         # target_probs = np.array([[1, 2, 3], [5, 6, 7]])
-        target_probs = np.array([[1, 5], [2, 6], [3, 7]])
+        target_probs = [[a, d], [b, e], [c, f]]
         # target_values = np.array([[1, 2, 3], [5, 6, 7]])
-        target_values = np.array([[1, 5], [2, 6], [3, 7]])
+        target_values = [[a, d], [b, e], [c, f]]
 
         # target_rewards = np.array([[1, 2, 3], [5, 6, 7]])
-        target_rewards = np.array([[1, 5], [2, 6], [3, 7]])
+        target_rewards = [[a, d], [b, e], [c, f]]
 
         # 2 particles for 3 time steps
         input_data = [
-                [1, 1, 1],
-                [5, 5, 5],
-                [2, 2, 2],
-                [6, 6, 6],
-                [3, 3, 3],
-                [7, 7, 7],
+                [a, a, a, a],
+                [d, d, d, d],
+                [b, b, b, b],
+                [e, e, e, e],
+                [c, c, c, c],
+                [f, f, f, f],
             ]
 
-        probs, values, rewards, _ = self.rl_trainer._format_episode_data(input_data)
+        probs, values, rewards, _, time = self.rl_trainer._format_episode_data(
+            input_data
+        )
 
         np.testing.assert_array_equal(target_probs, probs)
         np.testing.assert_array_equal(target_values, values)
         np.testing.assert_array_equal(target_rewards, rewards)
+        assert time == 3
