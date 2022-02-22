d5aa62d6a6f9ec2feb4fb275665bd6c718abe4c8
SamTov <tovey.samuel@gmail.com>
2022-02-22 10:02:30 +0100

    Check and update all RL tests

--- CI/unit_tests/losses/test_policy_gradient_loss.py
+++ CI/unit_tests/losses/test_policy_gradient_loss.py
@@ -4,12 +4,7 @@ Run a unit test on the loss module.
 import pytest
 import torch
 
-from swarmrl.losses.policy_gradient_loss import (
-    PolicyGradientLoss,
-    compute_actor_loss,
-    compute_critic_loss,
-    compute_true_value_function,
-)
+from swarmrl.losses.policy_gradient_loss import PolicyGradientLoss
 
 
 class TestLoss:
@@ -27,19 +22,24 @@ class TestLoss:
 
         """
         cls.loss = PolicyGradientLoss()
-        cls.rewards = torch.tensor(
-            [
-                [1, 1, 1, 1, 1],
-                [2, 2, 2, 2, 2],
-                [3, 3, 3, 3, 3],
-                [4, 4, 4, 4, 4],
-                [5, 5, 5, 5, 5],
-                [6, 6, 6, 6, 6],
-                [7, 7, 7, 7, 7],
-                [8, 8, 8, 8, 8],
-                [9, 9, 9, 9, 9],
-                [10, 10, 10, 10, 10],
-            ]
+        cls.loss.n_particles = 10
+        cls.loss.n_time_steps = 5
+        cls.rewards = torch.transpose(
+            torch.tensor(
+                [
+                    [1, 1, 1, 1, 1],
+                    [2, 2, 2, 2, 2],
+                    [3, 3, 3, 3, 3],
+                    [4, 4, 4, 4, 4],
+                    [5, 5, 5, 5, 5],
+                    [6, 6, 6, 6, 6],
+                    [7, 7, 7, 7, 7],
+                    [8, 8, 8, 8, 8],
+                    [9, 9, 9, 9, 9],
+                    [10, 10, 10, 10, 10],
+                ]
+            ),
+            0, 1
         )
 
     def test_expected_returns(self):
@@ -51,21 +51,24 @@ class TestLoss:
         Checks the actual values return in the discounted returns without
         standardization and with the simple case of gamma=1
         """
-        target_values = torch.tensor(
-            [
-                [5.0, 4.0, 3.0, 2.0, 1.0],
-                [10.0, 8.0, 6.0, 4.0, 2.0],
-                [15.0, 12.0, 9.0, 6.0, 3.0],
-                [20.0, 16.0, 12.0, 8.0, 4.0],
-                [25.0, 20.0, 15.0, 10.0, 5.0],
-                [30.0, 24.0, 18.0, 12.0, 6.0],
-                [35.0, 28.0, 21.0, 14.0, 7.0],
-                [40.0, 32.0, 24.0, 16.0, 8.0],
-                [45.0, 36.0, 27.0, 18.0, 9.0],
-                [50.0, 40.0, 30.0, 20.0, 10.0],
-            ]
+        target_values = torch.transpose(
+            torch.tensor(
+                [
+                    [5.0, 4.0, 3.0, 2.0, 1.0],
+                    [10.0, 8.0, 6.0, 4.0, 2.0],
+                    [15.0, 12.0, 9.0, 6.0, 3.0],
+                    [20.0, 16.0, 12.0, 8.0, 4.0],
+                    [25.0, 20.0, 15.0, 10.0, 5.0],
+                    [30.0, 24.0, 18.0, 12.0, 6.0],
+                    [35.0, 28.0, 21.0, 14.0, 7.0],
+                    [40.0, 32.0, 24.0, 16.0, 8.0],
+                    [45.0, 36.0, 27.0, 18.0, 9.0],
+                    [50.0, 40.0, 30.0, 20.0, 10.0],
+                ]
+            ),
+            0, 1
         )
-        value_function = compute_true_value_function(
+        value_function = self.loss.compute_true_value_function(
             rewards=self.rewards, gamma=1, standardize=False
         )
         torch.testing.assert_allclose(target_values, value_function)
@@ -78,17 +81,17 @@ class TestLoss:
         -----
         Test that the expected returns are correct.
         """
-        discounted_returns = compute_true_value_function(rewards=self.rewards)
-        torch.testing.assert_allclose(torch.mean(discounted_returns[0]).numpy(), 0.0)
-        torch.testing.assert_allclose(torch.mean(discounted_returns[1]).numpy(), 0.0)
-        torch.testing.assert_allclose(torch.mean(discounted_returns[2]).numpy(), 0.0)
-        torch.testing.assert_allclose(torch.mean(discounted_returns[3]).numpy(), 0.0)
-        torch.testing.assert_allclose(torch.mean(discounted_returns[4]).numpy(), 0.0)
-        torch.testing.assert_allclose(torch.std(discounted_returns[0]).numpy(), 1.0)
-        torch.testing.assert_allclose(torch.std(discounted_returns[1]).numpy(), 1.0)
-        torch.testing.assert_allclose(torch.std(discounted_returns[2]).numpy(), 1.0)
-        torch.testing.assert_allclose(torch.std(discounted_returns[3]).numpy(), 1.0)
-        torch.testing.assert_allclose(torch.std(discounted_returns[4]).numpy(), 1.0)
+        discounted_returns = self.loss.compute_true_value_function(rewards=self.rewards)
+        torch.testing.assert_allclose(torch.mean(discounted_returns[:, 0]).numpy(), 0.0)
+        torch.testing.assert_allclose(torch.mean(discounted_returns[:, 1]).numpy(), 0.0)
+        torch.testing.assert_allclose(torch.mean(discounted_returns[:, 2]).numpy(), 0.0)
+        torch.testing.assert_allclose(torch.mean(discounted_returns[:, 3]).numpy(), 0.0)
+        torch.testing.assert_allclose(torch.mean(discounted_returns[:, 4]).numpy(), 0.0)
+        torch.testing.assert_allclose(torch.std(discounted_returns[:, 0]).numpy(), 1.0)
+        torch.testing.assert_allclose(torch.std(discounted_returns[:, 1]).numpy(), 1.0)
+        torch.testing.assert_allclose(torch.std(discounted_returns[:, 2]).numpy(), 1.0)
+        torch.testing.assert_allclose(torch.std(discounted_returns[:, 3]).numpy(), 1.0)
+        torch.testing.assert_allclose(torch.std(discounted_returns[:, 4]).numpy(), 1.0)
 
     def test_actor_loss(self):
         """
@@ -98,36 +101,61 @@ class TestLoss:
         -------
 
         """
-        rewards = torch.tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
+        # Update class parameters for new case.
+        self.loss.n_particles = 2
+        self.loss.n_time_steps = 5
 
-        action_probs = torch.nn.Softmax()(
-            torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]])
+        rewards = torch.transpose(
+            torch.tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]), 0, 1
         )
 
-        predicted_rewards = torch.tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
-        actor_loss = compute_actor_loss(
+        action_probs = torch.nn.Softmax()(
+            torch.transpose(
+                torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]]),
+                0, 1
+            )
+        )
+
+        predicted_rewards = torch.transpose(
+            torch.tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]), 0, 1
+        )
+        actor_loss = self.loss.compute_actor_loss(
             log_probs=action_probs, rewards=rewards, predicted_values=predicted_rewards
         )
 
-        assert float(actor_loss[0].numpy()) == pytest.approx(5.455, 0.001)
+        assert float(actor_loss[0].numpy()) == pytest.approx(7.5, 0.001)
         assert len(actor_loss) == 2
         assert actor_loss[0] == actor_loss[1]
+
+        # Reset to defaults for non-linear deployment case.
+        self.loss.n_particles = 10
+        self.loss.n_time_steps = 5
 
     def test_critic_loss(self):
         """
         Test the critic loss for 2 particles.
-
-        Returns
-        -------
-
         """
-        rewards = torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]])
-        predicted_rewards = torch.tensor(
-            [[2.0, 3.0, 4.0, 5.0, 6.0], [2.0, 3.0, 4.0, 5.0, 6.0]]
+        # Update class parameters for new case.
+        self.loss.n_particles = 2
+        self.loss.n_time_steps = 5
+
+        rewards = torch.transpose(
+            torch.tensor([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]]), 0, 1
         )
-        critic_loss = compute_critic_loss(
+
+        predicted_rewards = torch.transpose(
+            torch.tensor(
+                [[2.0, 3.0, 4.0, 5.0, 6.0], [2.0, 3.0, 4.0, 5.0, 6.0]]
+            ),
+            0, 1
+        )
+        critic_loss = self.loss.compute_critic_loss(
             rewards=rewards, predicted_rewards=predicted_rewards
         )
         assert len(critic_loss) == 2
         assert critic_loss[0] == critic_loss[1]
-        assert critic_loss[0] == pytest.approx(3.5, 0.001)
+        assert critic_loss[0] == pytest.approx(17.5, 0.001)
+
+        # Reset to defaults for asymmetric deployment case.
+        self.loss.n_particles = 10
+        self.loss.n_time_steps = 5
--- CI/unit_tests/tasks/test_find_location.py
+++ CI/unit_tests/tasks/test_find_location.py
@@ -3,6 +3,7 @@ Test the find origin task.
 """
 import numpy as np
 import pytest
+import torch
 
 from swarmrl.tasks.searching.find_location import FindLocation
 
@@ -23,9 +24,9 @@ class TestFindLocation:
         """
         Test the reward for the default unit cube and origin.
         """
-        bad_reward = self.task.compute_reward(np.array([1, 1, 0]))
-        good_reward = self.task.compute_reward(np.array([0, 0, 0]))
-        mixed_reward = self.task.compute_reward(np.array([1.0, 0.0, 0]))
+        bad_reward = self.task.compute_reward(torch.tensor([1, 1, 0]))
+        good_reward = self.task.compute_reward(torch.tensor([0, 0, 0]))
+        mixed_reward = self.task.compute_reward(torch.tensor([1.0, 0.0, 0]))
 
         assert bad_reward == 0.0
         assert good_reward == pytest.approx(1.414, 0.001)
@@ -36,9 +37,9 @@ class TestFindLocation:
         Test the reward for a shifted box.
         """
         self.task.location = np.array([0.9, 0.9, 0.0])
-        good_reward = self.task.compute_reward(np.array([1, 1, 0]))
-        bad_reward = self.task.compute_reward(np.array([0, 0, 0]))
-        mixed_reward = self.task.compute_reward(np.array([1.0, 0.0, 0]))
+        good_reward = self.task.compute_reward(torch.tensor([1, 1, 0]))
+        bad_reward = self.task.compute_reward(torch.tensor([0, 0, 0]))
+        mixed_reward = self.task.compute_reward(torch.tensor([1.0, 0.0, 0]))
 
         assert good_reward == pytest.approx(1.273, 0.001)
         assert bad_reward == pytest.approx(0.1414, 0.001)
@@ -50,12 +51,12 @@ class TestFindLocation:
 
         Should return the same results as the first test on a much larger box.
         """
-        self.task.location = np.array([0.0, 0.0, 0.0])
-        self.task.side_length = np.array([1000.0, 1000.0, 1000.0])
+        self.task.location = torch.tensor([0.0, 0.0, 0.0])
+        self.task.side_length = torch.tensor([1000.0, 1000.0, 1000.0])
         self.task._compute_max_distance()  # recompute this attribute
-        bad_reward = self.task.compute_reward(np.array([1000.0, 1000.0, 0.0]))
-        good_reward = self.task.compute_reward(np.array([0, 0, 0.0]))
-        mixed_reward = self.task.compute_reward(np.array([1000.0, 0.0, 0.0]))
+        bad_reward = self.task.compute_reward(torch.tensor([1000.0, 1000.0, 0.0]))
+        good_reward = self.task.compute_reward(torch.tensor([0, 0, 0.0]))
+        mixed_reward = self.task.compute_reward(torch.tensor([1000.0, 0.0, 0.0]))
 
         assert bad_reward == 0.0
         assert good_reward == pytest.approx(1.414, 0.001)
