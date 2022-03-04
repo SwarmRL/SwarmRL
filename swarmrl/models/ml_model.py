c8372fd0ce39945ff3ef28d3cd073b62ba320cc0
tmerkt <tobias.merkt@live.de>
2022-03-04 13:41:14 +0100

    Added 20% randomness to the selection of actions

--- swarmrl/models/ml_model.py
+++ swarmrl/models/ml_model.py
@@ -110,6 +110,7 @@ class MLModel(InteractionModel):
             action_idx = action_distribution.sample()
             # action_log_prob = action_distribution.log_prob(action_idx)
 
+            print(self.actions[list(self.actions)[action_idx.item()]])
             actions.append(self.actions[list(self.actions)[action_idx.item()]])
 
         return actions
