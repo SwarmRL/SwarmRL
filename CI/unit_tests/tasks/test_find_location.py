47b520a07f199c59350b8c15f46634bdbc8e675f
SamTov <tovey.samuel@gmail.com>
2022-03-16 09:11:09 -0500

    fix task tests

--- CI/unit_tests/tasks/test_find_location.py
+++ CI/unit_tests/tasks/test_find_location.py
@@ -24,9 +24,9 @@ class TestFindLocation:
         """
         Test the reward for the default unit cube and origin.
         """
-        bad_reward = self.task.compute_reward(torch.tensor([1, 1, 0]))
-        good_reward = self.task.compute_reward(torch.tensor([0, 0, 0]))
-        mixed_reward = self.task.compute_reward(torch.tensor([1.0, 0.0, 0]))
+        bad_reward = self.task(torch.tensor([1, 1, 0]))
+        good_reward = self.task(torch.tensor([0, 0, 0]))
+        mixed_reward = self.task(torch.tensor([1.0, 0.0, 0]))
 
         assert bad_reward == 0.0
         assert good_reward == pytest.approx(1.414, 0.001)
@@ -37,9 +37,9 @@ class TestFindLocation:
         Test the reward for a shifted box.
         """
         self.task.location = np.array([0.9, 0.9, 0.0])
-        good_reward = self.task.compute_reward(torch.tensor([1, 1, 0]))
-        bad_reward = self.task.compute_reward(torch.tensor([0, 0, 0]))
-        mixed_reward = self.task.compute_reward(torch.tensor([1.0, 0.0, 0]))
+        good_reward = self.task(torch.tensor([1, 1, 0]))
+        bad_reward = self.task(torch.tensor([0, 0, 0]))
+        mixed_reward = self.task(torch.tensor([1.0, 0.0, 0]))
 
         assert good_reward == pytest.approx(1.273, 0.001)
         assert bad_reward == pytest.approx(0.1414, 0.001)
@@ -54,9 +54,9 @@ class TestFindLocation:
         self.task.location = torch.tensor([0.0, 0.0, 0.0])
         self.task.side_length = torch.tensor([1000.0, 1000.0, 1000.0])
         self.task._compute_max_distance()  # recompute this attribute
-        bad_reward = self.task.compute_reward(torch.tensor([1000.0, 1000.0, 0.0]))
-        good_reward = self.task.compute_reward(torch.tensor([0, 0, 0.0]))
-        mixed_reward = self.task.compute_reward(torch.tensor([1000.0, 0.0, 0.0]))
+        bad_reward = self.task(torch.tensor([1000.0, 1000.0, 0.0]))
+        good_reward = self.task(torch.tensor([0, 0, 0.0]))
+        mixed_reward = self.task(torch.tensor([1000.0, 0.0, 0.0]))
 
         assert bad_reward == 0.0
         assert good_reward == pytest.approx(1.414, 0.001)
