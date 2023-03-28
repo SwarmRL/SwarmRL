bd2dea198bba8a3983d05f39ecf0437b2c277f91
SamTov <tovey.samuel@gmail.com>
2023-03-28 14:36:31 +0200

    Remove old files

--- swarmrl/tasks/searching/__init__.py
+++ swarmrl/tasks/searching/__init__.py
@@ -1,12 +1,8 @@
 """
 Modules for search algorithms.
 """
-from swarmrl.tasks.searching._gradient_sensing_vision_cone import (
-    GradientSensingVisionCone,
-)
 from swarmrl.tasks.searching.gradient_sensing import GradientSensing
 
 __all__ = [
     GradientSensing.__name__,
-    GradientSensingVisionCone.__name__,
 ]
