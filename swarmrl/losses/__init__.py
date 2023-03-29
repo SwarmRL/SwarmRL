ffb29c182ba934ea966c5aad9556b853bc1fd53a
David <zimmerda96@web.de>
2023-03-29 18:12:44 +0200

    working on stuff

--- swarmrl/losses/__init__.py
+++ swarmrl/losses/__init__.py
@@ -3,10 +3,10 @@ Module implementing different loss models.
 """
 from swarmrl.losses.policy_gradient_loss import PolicyGradientLoss
 from swarmrl.losses.proximal_policy_loss import ProximalPolicyLoss
-from swarmrl.losses.proximal_policy_loss_shared import ProximalPolicyLossShared
+from swarmrl.losses.proximal_policy_loss_shared import SharedProximalPolicyLoss
 
 __all__ = [
     PolicyGradientLoss.__name__,
     ProximalPolicyLoss.__name__,
-    ProximalPolicyLossShared.__name__,
+    SharedProximalPolicyLoss.__name__,
 ]
