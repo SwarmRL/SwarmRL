af8cabeadbeda8a299b3a85c1d109e350e05ad04
mu-gaimann <35263162+mu-gaimann@users.noreply.github.com>
2023-07-28 18:49:14 +0200

    added resobee engine example, zqm communication works now

--- swarmrl/engine/resobee.py
+++ swarmrl/engine/resobee.py
@@ -6,9 +6,18 @@ from swarmrl.models.interaction_model import InteractionModel
 
 import numpy as np
 import zmq
-import time
 import yaml
 import os
+import subprocess
+
+
+class Population:
+    def __init__(self, n_agents):
+        self.indices = np.zeros(n_agents)
+        self.types = np.zeros(n_agents)
+        self.unwrapped_positions = np.zeros((n_agents, 2))
+        self.velocities = np.zeros((n_agents, 2))
+        self.directors = np.zeros((n_agents, 2))
 
 
 class ResoBee(Engine):
@@ -16,15 +25,56 @@ class ResoBee(Engine):
     Child class for the ResoBee Engine.
     """
 
-    def __init__(self, executable_path, config_dir):
-        self.executable_path = executable_path
-        config_path = os.path.join(config_dir, 'config.yaml')
-        self.config = yaml.safe_load(open(config_path, 'r'))
+    def __init__(self, resobee_executable, config_dir):
+        self.resobee_executable = resobee_executable
+        self.config_dir = config_dir
+        self.config = yaml.safe_load(open(os.path.join(config_dir, 'config.yaml'), 'r'))
+        self.population = Population(self.config["n_agents"])
 
         # initialize the zmq socket for communication with ResoBee
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
-        self.socket.bind(self.config.tcp_address)
+        print("Binding to tcp address: ", self.config["tcp_address"])
+        self.socket.bind(str(self.config["tcp_address"]))
+
+    def __del__(self):
+        self.socket.close()
+        self.context.term()
+
+    def receive_state(self):
+        print("Listening for requests from ResoBee client...")
+        message = self.socket.recv_json()
+
+        print("Received data from ResoBee client.")
+        try:
+            self.population.indices = np.array(message["indices"])
+            self.population.unwrapped_positions = np.array(list(zip(message["x"], message["y"])))
+            self.population.velocities = np.array(list(zip(message["v_x"], message["v_y"])))
+        except KeyError:
+            print("Received an incomplete simulation state from ResoBee client.")
+
+        try:
+            simulation_is_finished = message["is_finished"]
+        except KeyError:
+            print("Received no simulation completion state from ResoBee client.")
+            raise KeyError
+
+        return simulation_is_finished
+
+    def compute_action(self, force_model) -> tuple[list, list]:
+        # todo: implement this, for now this is random
+        forces_x = np.random.rand(self.config["n_agents"]).tolist()
+        forces_y = np.random.rand(self.config["n_agents"]).tolist()
+
+        return forces_x, forces_y
+
+    def send_action(self, forces_x: list, forces_y: list):
+        print("Sending action (forces) to ResoBee client...")
+        ret = {
+            "f_x": forces_x,
+            "f_y": forces_y
+        }
+        self.socket.send_json(ret)
 
     def integrate(
             self,
@@ -41,15 +91,24 @@ class ResoBee(Engine):
             A an instance of swarmrl.models.interaction_model.InteractionModel
         """
 
-        print("Computing action...")
-        time.sleep(1)
+        # start the ResoBee engine
+        try:
+            process = subprocess.Popen([self.resobee_executable, self.config_dir])
 
-        print("Sending action (forces) to ResoBee client...")
-        ret = {
-            "f_x": np.random.rand(self.config.n_agents).tolist(),
-            "f_y": np.random.rand(self.config.n_agents).tolist()
-        }
-        self.socket.send_json(ret)
+            # run the ResoBee engine until it finishes
+            simulation_is_finished = False
+
+            while simulation_is_finished is False:
+                simulation_is_finished = self.receive_state()
+                f_x, f_y = self.compute_action(force_model)
+                self.send_action(f_x, f_y)
+
+                # check if the ResoBee engine has finished
+                if simulation_is_finished:
+                    print("ResoBee simulation successfully completed.")
+                    break
+        finally:
+            process.kill()
 
     def get_particle_data(self) -> dict:
         """
@@ -59,20 +118,10 @@ class ResoBee(Engine):
         The particle data is fetched from the C++ engine and converted to a dict.
         """
 
-        print("Listening for requests from ResoBee client...")
-        message = self.socket.recv_json()
-
-        print("Received state (population) from ResoBee client: ", message)
-        indices = np.array(message["indexes"])
-        types = np.random.rand(self.config.n_agents)  # zeros for now
-        unwrapped_positions = np.array(list(zip(message["x"], message["y"])))
-        velocities = np.array(list(zip(message["v_x"], message["v_y"])))
-        directors = np.random.rand(self.config.n_agents, 2)  # zeros for now
-
         return {
-            "Id": indices,
-            "Type": types,
-            "Unwrapped_Positions": unwrapped_positions,
-            "Velocities": velocities,
-            "Directors": directors,
+            "Id": self.population.indices,
+            "Type": self.population.types,
+            "Unwrapped_Positions": self.population.unwrapped_positions,
+            "Velocities": self.population.velocities,
+            "Directors": self.population.directors,
         }
