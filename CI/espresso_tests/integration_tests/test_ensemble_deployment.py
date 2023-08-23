a52e333770b887e784168ea6e4b06d411bdf8a41
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2023-08-23 16:52:37 +0200

    add anisotropic particles

--- CI/espresso_tests/integration_tests/test_ensemble_deployment.py
+++ CI/espresso_tests/integration_tests/test_ensemble_deployment.py
@@ -1,7 +1,7 @@
 """
 Test that ensembled deployment runs.
 """
-import shutil
+import tempfile
 import unittest as ut
 
 import flax.linen as nn
@@ -14,7 +14,7 @@ from swarmrl.models.interaction_model import Action
 
 
 # Helper definitions.
-def get_simulation_runner():
+def get_simulation_runner(folder):
     """
     Collect a simulation runner.
     """
@@ -40,7 +40,7 @@ def get_simulation_runner():
         md_params=md_params,
         n_dims=2,
         seed=seed,
-        out_folder=simulation_name,
+        out_folder=folder + "/" + simulation_name,
         write_chunk_size=100,
     )
 
@@ -90,98 +90,87 @@ class TestGeneticTraining(ut.TestCase):
     Test suite for the genetic training.
     """
 
-    def setUp(self):
-        """
-        Prepare the test.
-        """
-
-        # Exploration policy
-        exploration_policy = srl.exploration_policies.RandomExploration(probability=0.1)
-
-        # Sampling strategy
-        sampling_strategy = srl.sampling_strategies.GumbelDistribution()
-
-        # Value function
-        value_function = srl.value_functions.ExpectedReturns(
-            gamma=0.99, standardize=True
-        )
-
-        # Set the task
-        task = srl.tasks.searching.GradientSensing(
-            source=np.array([500.0, 500.0, 0.0]),
-            decay_function=scale_function,
-            reward_scale_factor=10,
-            box_length=np.array([1000.0, 1000.0, 1000]),
-        )
-        observable = srl.observables.ConcentrationField(
-            source=np.array([500.0, 500.0, 0.0]),
-            decay_fn=scale_function,
-            scale_factor=10,
-            box_length=np.array([1000.0, 1000.0, 1000]),
-        )
-
-        # Define the loss model
-        loss = srl.losses.PolicyGradientLoss(value_function=value_function)
-
-        actor = srl.networks.FlaxModel(
-            flax_model=ActorNet(),
-            optimizer=optax.adam(learning_rate=0.001),
-            input_shape=(1,),
-            sampling_strategy=sampling_strategy,
-            exploration_policy=exploration_policy,
-        )
-
-        critic = srl.networks.FlaxModel(
-            flax_model=CriticNet(),
-            optimizer=optax.adam(learning_rate=0.001),
-            input_shape=(1,),
-        )
-
-        translate = Action(force=10.0)
-        rotate_clockwise = Action(torque=np.array([0.0, 0.0, 10.0]))
-        rotate_counter_clockwise = Action(torque=np.array([0.0, 0.0, -10.0]))
-        do_nothing = Action()
-
-        actions = {
-            "RotateClockwise": rotate_clockwise,
-            "Translate": translate,
-            "RotateCounterClockwise": rotate_counter_clockwise,
-            "DoNothing": do_nothing,
-        }
-
-        protocol = srl.rl_protocols.ActorCritic(
-            particle_type=0,
-            actor=actor,
-            critic=critic,
-            task=task,
-            observable=observable,
-            actions=actions,
-        )
-
-        rl_trainer = srl.gyms.Gym(
-            [protocol],
-            loss,
-        )
-        self.training_routine = srl.training_routines.EnsembleTraining(
-            rl_trainer,
-            get_simulation_runner,
-            number_of_ensembles=20,
-            n_episodes=50,
-            n_parallel_jobs=2,
-            episode_length=20,
-        )
-
     def test_run(self):
-        """
-        Test that the code runs correctly.
-        """
-        self.training_routine.train_ensemble()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Exploration policy
+            exploration_policy = srl.exploration_policies.RandomExploration(
+                probability=0.1
+            )
 
-    def tearDown(self):
-        """
-        Clean up after running.
-        """
-        shutil.rmtree("ensembled-training")
+            # Sampling strategy
+            sampling_strategy = srl.sampling_strategies.GumbelDistribution()
+
+            # Value function
+            value_function = srl.value_functions.ExpectedReturns(
+                gamma=0.99, standardize=True
+            )
+
+            # Set the task
+            task = srl.tasks.searching.GradientSensing(
+                source=np.array([500.0, 500.0, 0.0]),
+                decay_function=scale_function,
+                reward_scale_factor=10,
+                box_length=np.array([1000.0, 1000.0, 1000]),
+            )
+            observable = srl.observables.ConcentrationField(
+                source=np.array([500.0, 500.0, 0.0]),
+                decay_fn=scale_function,
+                scale_factor=10,
+                box_length=np.array([1000.0, 1000.0, 1000]),
+            )
+
+            # Define the loss model
+            loss = srl.losses.PolicyGradientLoss(value_function=value_function)
+
+            actor = srl.networks.FlaxModel(
+                flax_model=ActorNet(),
+                optimizer=optax.adam(learning_rate=0.001),
+                input_shape=(1,),
+                sampling_strategy=sampling_strategy,
+                exploration_policy=exploration_policy,
+            )
+
+            critic = srl.networks.FlaxModel(
+                flax_model=CriticNet(),
+                optimizer=optax.adam(learning_rate=0.001),
+                input_shape=(1,),
+            )
+
+            translate = Action(force=10.0)
+            rotate_clockwise = Action(torque=np.array([0.0, 0.0, 10.0]))
+            rotate_counter_clockwise = Action(torque=np.array([0.0, 0.0, -10.0]))
+            do_nothing = Action()
+
+            actions = {
+                "RotateClockwise": rotate_clockwise,
+                "Translate": translate,
+                "RotateCounterClockwise": rotate_counter_clockwise,
+                "DoNothing": do_nothing,
+            }
+
+            protocol = srl.rl_protocols.ActorCritic(
+                particle_type=0,
+                actor=actor,
+                critic=critic,
+                task=task,
+                observable=observable,
+                actions=actions,
+            )
+
+            rl_trainer = srl.gyms.Gym(
+                [protocol],
+                loss,
+            )
+            self.training_routine = srl.training_routines.EnsembleTraining(
+                rl_trainer,
+                get_simulation_runner(temp_dir),
+                number_of_ensembles=20,
+                n_episodes=50,
+                n_parallel_jobs=2,
+                episode_length=20,
+            )
+
+            self.training_routine.train_ensemble()
 
 
 if __name__ == "__main__":
