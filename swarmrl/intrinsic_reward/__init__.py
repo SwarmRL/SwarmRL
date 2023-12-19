626485414d738e8841bdc83eb390252e94dda3c6
knikolaou <knikolaou@icp.uni-stuttgart.de>
2023-12-19 19:39:23 +0100

    add init to the intrinsic reward module

---
+++ swarmrl/intrinsic_reward/__init__.py
@@ -0,0 +1,14 @@
+"""
+Package intrinsic_rewards for Agents.
+"""
+
+from swarmrl.intrinsic_reward.intrinsic_reward import IntrinsicReward
+from swarmrl.intrinsic_reward.rnd_configs import RNDArchitecture, RNDConfig
+from swarmrl.intrinsic_reward.random_network_distillation import RNDReward
+
+__all__ = [
+    IntrinsicReward.__name__,
+    RNDArchitecture.__name__,
+    RNDConfig.__name__,
+    RNDReward.__name__,
+]
