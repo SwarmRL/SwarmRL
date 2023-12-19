a95633bc18e596c3d40ce0dc76cca42071d8ccb0
knikolaou <knikolaou@icp.uni-stuttgart.de>
2023-12-19 20:26:04 +0100

    run isort

--- swarmrl/intrinsic_reward/__init__.py
+++ swarmrl/intrinsic_reward/__init__.py
@@ -3,8 +3,8 @@ Package intrinsic_rewards for Agents.
 """
 
 from swarmrl.intrinsic_reward.intrinsic_reward import IntrinsicReward
-from swarmrl.intrinsic_reward.rnd_configs import RNDArchitecture, RNDConfig
 from swarmrl.intrinsic_reward.random_network_distillation import RNDReward
+from swarmrl.intrinsic_reward.rnd_configs import RNDArchitecture, RNDConfig
 
 __all__ = [
     IntrinsicReward.__name__,
