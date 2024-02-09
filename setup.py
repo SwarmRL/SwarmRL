632759a92f458434e08dff06cf67e94e7d691d3a
knikolaou <knikolaou@icp.uni-stuttgart.de>
2024-02-09 17:00:00 +0100

    change reqs

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ Configuration file for the package.
 """
 
 from setuptools import setup, find_packages
-from setuptools.command.install import install as InstallCommand
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
