bc7451dad3ce898af55c1773092759d0bc2c2b04
knikolaou <knikolaou@icp.uni-stuttgart.de>
2024-02-12 16:24:39 +0100

    update python requirements

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=required,
     extras_require={
         "rnd": [
