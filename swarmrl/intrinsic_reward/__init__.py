415777c01a2625e710e987f6f1e29c281961b845
SamTov <tovey.samuel@gmail.com>
2024-02-28 09:49:56 +0100

    Make ZnNL import optional

--- swarmrl/intrinsic_reward/__init__.py
+++ swarmrl/intrinsic_reward/__init__.py
@@ -1,14 +1,29 @@
 """
 Package intrinsic_rewards for Agents.
 """
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 from swarmrl.intrinsic_reward.intrinsic_reward import IntrinsicReward
-from swarmrl.intrinsic_reward.random_network_distillation import RNDReward
-from swarmrl.intrinsic_reward.rnd_configs import RNDArchitecture, RNDConfig
+
+try:
+    from swarmrl.intrinsic_reward.random_network_distillation import RNDReward
+    from swarmrl.intrinsic_reward.rnd_configs import RNDArchitecture, RNDConfig
+
+    __all__ = [
+        IntrinsicReward.__name__,
+        RNDArchitecture.__name__,
+        RNDConfig.__name__,
+        RNDReward.__name__,
+    ]
+except ImportError:
+    logger.info(
+        "Could not find optional packages, certain features might be unavailable."
+        )
+    pass
 
 __all__ = [
-    IntrinsicReward.__name__,
-    RNDArchitecture.__name__,
-    RNDConfig.__name__,
-    RNDReward.__name__,
-]
+        IntrinsicReward.__name__,
+    ]
