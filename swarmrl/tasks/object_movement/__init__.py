e8aa695556dba8b26aae505d52ae5418ad6ec08d
Marcel Gern <marcel@familie-gern.de>
2024-03-11 11:45:22 +0100

    removed ResoBeeSwarm task

--- swarmrl/tasks/object_movement/__init__.py
+++ swarmrl/tasks/object_movement/__init__.py
@@ -3,6 +3,5 @@ Init file for object movement tasks.
 """
 
 from swarmrl.tasks.object_movement.rod_rotation import RotateRod
-from swarmrl.tasks.object_movement.resobee_swarm import ResoBeeSwarm
 
-__all__ = [RotateRod.__name__, ResoBeeSwarm.__name__]
+__all__ = [RotateRod.__name__]
