9e274fc703eb8a2c2b949f7bec2216dc7afea41d
Tobias Ertl <tobi.ertl@t-online.de>
2024-09-06 13:53:26 +0200

    Add caution comment to compute_forces

--- swarmrl/utils/colloid_utils.py
+++ swarmrl/utils/colloid_utils.py
@@ -31,6 +31,10 @@ def compute_forces(r: jnp.ndarray, director: jnp.ndarray) -> jnp.ndarray:
     """
     Compute the energy between two colloids.
 
+    Use this function with caution, as too big values in r can result
+    in NaN-Values after the jax.grad() calculation. This can happen if
+    the norm of these entries surpasses 1.5e+03.
+
     This uses a WCA potential to compute a relative force between
     two colloids. It is not physical.
     The method itself implements an energy computation which then uses
