Fix torque calculation

swarmrl/tasks/object_movement/rod_torque.py
@@ -83,7 +83,6 @@ class RodTorque(Task):
         colloid_positions: np.ndarray,
         colloid_directors: np.ndarray,
         rod_positions: np.ndarray,
-        rod_directors: np.ndarray,
     )-> np.ndarray:
         """
         Compute the torques on the rod.
@@ -96,21 +95,19 @@ class RodTorque(Task):
                 Directors of the colloids.
         rod_positions : np.ndarray (n_rod, 3)
                 Positions of the rod particles.
-        rod_directors : np.ndarray (n_rod, 3)
-                Directors of the rod particles.
 
         Returns
         -------
         torques : np.ndarray (n_colloids, )
                 Torques on the rod for each colloid.
         """
-        torques = self.decomp_fn(colloid_positions, colloid_directors, rod_positions, rod_directors)[:,2]
+        torques = self.decomp_fn(colloid_positions, colloid_directors, rod_positions)[:,2]
         return torques
 
     def _torque_partition(
         self,
         colloids_torques_on_rod: np.ndarray,
-        )-> np.ndarray:                                       # Negative Torques entsprechen CCW-Rotation Also muss das Vorzeichen fuer Belohnungen einmal umgedreht werden
+        )-> np.ndarray:                                       
         """
         Remove rewards for torque in the wrong direction and apply the scaling.
 
@@ -122,14 +119,14 @@ class RodTorque(Task):
         Returns
         -------
         torques_in_direction : np.ndarray (n_colloids, )
-                Torques on the rod for each colloid with wrong directions set to 0.
+                Torques on the rod for each colloid with wrong directions set to 0 and scaled accordig to the angular_velocity_scale.
         """
         if self.angular_velocity_scale > 0.0:
             torques_in_direction = colloids_torques_on_rod.at[colloids_torques_on_rod > 0.0].set(0.0)
         else:
             torques_in_direction = colloids_torques_on_rod.at[colloids_torques_on_rod < 0.0].set(0.0)
 
-        return torques_in_direction * -1 * self.angular_velocity_scale
+        return torques_in_direction * -1 * self.angular_velocity_scale  # Sign of the torques has to be inverted to avoid negativ rewards
 
     def __call__(self, colloids: List[Colloid]):
         """
@@ -160,8 +157,8 @@ class RodTorque(Task):
         colloid_positions = np.array([colloid.pos for colloid in chosen_colloids])
         colloid_directors = np.array([colloid.director for colloid in chosen_colloids])
 
-        colloids_torque_on_rod = self._torque_on_rod(colloid_positions, colloid_directors, rod_positions, rod_directors)
-        
+        colloids_torque_on_rod = self._torque_on_rod(colloid_positions, colloid_directors, rod_positions)
+
         torques_in_turning_direction = self._torque_partition(colloids_torque_on_rod)
 
         return torques_in_turning_direction

swarmrl/trainers/episodic_trainer.py
@@ -123,7 +123,7 @@ class EpisodicTrainer(Trainer):
 
                 rewards.append(current_reward)
 
-                running_reward = np.round(np.mean(rewards[-10:]), 4)
+                running_reward = np.round(np.mean(rewards[-10:]), 6)
 
                 if save_best_network != None and running_reward > best_running_reward:
                     best_running_reward = running_reward
@@ -134,7 +134,7 @@ class EpisodicTrainer(Trainer):
                     task,
                     advance=1,
                     Episode=episode,
-                    current_reward=np.round(current_reward, 2),
+                    current_reward=np.round(current_reward, 4),
                     running_reward=running_reward,
                 )
                 self.engine.finalize()

swarmrl/utils/colloid_utils.py
@@ -82,7 +82,6 @@ def compute_distance_matrix(set_a, set_b):
 
     return distance_fn(set_a, set_b)
 
-
 @jax.jit
 def compute_torque(force, direction):
     """
@@ -134,8 +133,26 @@ def compute_torque_partition_on_rod(
 
     return torque_magnitude
 
+
 @jax.jit
-def compute_torque_on_rod(colloid_positions, colloid_directors, rod_positions, rod_directions):
+def compute_rod_particle_distances(rod_positions):
+    """
+    Compute the vectors between the middle of the rod to each colloid.
+
+    Parameters
+    ----------
+    rod_positions : Positions of all the rod colloids.
+    """
+
+    def _sub_compute(a, b):
+        return b - a
+
+    distance_fn = jax.vmap(_sub_compute, in_axes=(0, None))   
+
+    return distance_fn(rod_positions, rod_positions[0]) # rod_positions[0] is the middle of the rod
+
+@jax.jit
+def compute_torque_on_rod(colloid_positions, colloid_directors, rod_positions):
     """
     Compute the torque on a rod using a WCA potential.
 
@@ -147,8 +164,6 @@ def compute_torque_on_rod(colloid_positions, colloid_directors, rod_positions, r
         Directors of the colloids.
     rod_positions : jnp.ndarray (rod_particles, 3)
         Positions of the rod particles.
-    rod_directions : jnp.ndarray (rod_particles, 3)
-        Directors of the rod particles.
     """
     # (n_colloids, rod_particles, 3)
     distance_matrix = compute_distance_matrix(colloid_positions, rod_positions)
@@ -164,14 +179,16 @@ def compute_torque_on_rod(colloid_positions, colloid_directors, rod_positions, r
     colloid_rod_map = jax.vmap(compute_torque, in_axes=(0, 0))
     colloid_only_map = jax.vmap(colloid_rod_map, in_axes=(0, None))
 
-    torques = colloid_only_map(forces, rod_directions)
+    directions = compute_rod_particle_distances(rod_positions)  # Calculate the r vectors between the middle of the rod and each colloid. 
+    torques = colloid_only_map(forces, directions)              # This is used for the torque formula: T = r x F
+
     net_rod_torque = torques.sum(axis=1)
     torque_magnitude = jnp.linalg.norm(net_rod_torque, axis=-1) + 1e-8
     normalization_factors = torque_magnitude.sum()
     torque_partition = net_rod_torque / normalization_factors
 
     return torque_partition
-    
+
 def get_colloid_indices(colloids: List["Colloid"], p_type: int) -> List[int]:
     """
     Get the indices of the colloids in the observable of a specific type.