f97a8dd7ce88a9931f19c4cae28809d737087847
jdrotleff <151064457+jpd-de@users.noreply.github.com>
2025-01-08 17:09:55 +0100

    Update __init__.py
    Changed Checkpointer to BaseCheckpointer

--- swarmrl/checkpointers/__init__.py
+++ swarmrl/checkpointers/__init__.py
@@ -3,8 +3,8 @@ Package to hold all SwarmRL Checkpointers.
 """
 
 from swarmrl.checkpointers.backup_checkpointer import BackupCheckpointer
+from swarmrl.checkpointers.base_checkpointer import BaseCheckpointer
 from swarmrl.checkpointers.best_checkpointer import BestRewardCheckpointer
-from swarmrl.checkpointers.checkpointer import Checkpointer
 from swarmrl.checkpointers.goal_checkpointer import GoalCheckpointer
 from swarmrl.checkpointers.regular_checkpointer import RegularCheckpointer
 
@@ -13,5 +13,5 @@ __all__ = [
     BestRewardCheckpointer.__name__,
     GoalCheckpointer.__name__,
     RegularCheckpointer.__name__,
-    Checkpointer.__name__,
+    BaseCheckpointer.__name__,
 ]
