f607503d8911ee589e4e337278ca14ada46c6674
jdrotleff <151064457+jpd-de@users.noreply.github.com>
2025-03-06 16:02:32 +0100

    Create test_regular_checkpointer.py
    Add a test for the regular checkpointer module

---
+++ CI/unit_tests/checkpointers/test_regular_checkpointer.py
@@ -0,0 +1,49 @@
+"""
+Test the regular checkpointer module.
+"""
+
+import random
+
+import numpy as np
+
+from swarmrl.checkpointers.regular_checkpointer import RegularCheckpointer
+
+
+class TestBestCheckpointer:
+    """
+    Test suite for the regular checkpointer module.
+    """
+
+    @classmethod
+    def setup_class(cls):
+        """
+        Prepare the class for testing.
+        """
+        cls.random_integer = random.randint(2, 10)
+        cls.regular_checkpointer = RegularCheckpointer(save_interval=cls.random_integer)
+
+    def test_initialization(self):
+        """
+        Test the initialization of the regular checkpointer.
+        """
+        assert self.regular_checkpointer.save_interval == self.random_integer
+
+    def test_check_for_checkpoint(self):
+        """
+        Test the check for checkpoint method.
+        """
+        rewards = np.random.rand(self.random_integer * 5)
+
+        n_backups = 0
+        for episode_index in range(0, len(rewards)):
+            if (episode_index + 1) % self.random_integer == 0:
+                assert self.regular_checkpointer.check_for_checkpoint(
+                    rewards, episode_index
+                )
+                n_backups += 1
+            else:
+                assert not self.regular_checkpointer.check_for_checkpoint(
+                    rewards, episode_index
+                )
+
+        assert n_backups == len(rewards) // self.random_integer
